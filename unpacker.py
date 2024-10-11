e2e6eb1794ffb6ca40d943b5615bdfba892fb2d3
kamil <77517141+bosterptr@users.noreply.github.com>
2024-10-11 02:08:37 +0200

    Create unpacker.py

---
+++ unpacker.py
@@ -0,0 +1,20 @@
+import base64
+import zlib
+_ = lambda __: zlib.decompress(base64.b64decode(__[::-1]))
+
+def trim_string(input_str):
+    return input_str[13:-3]
+
+def execute_nested_base64(encoded_str):
+    decoded_str = encoded_str
+    while True:
+        try:
+            not_trimmed_decoded_str = eval(f'print((_)("{decoded_str.decode()}"))')
+            decoded_str = trim_string(not_trimmed_decoded_str)
+            print(decoded_str)
+        except Exception as e:
+            print(f"end: {e}")
+            break
+
+base64_string = b'BASE^$_HERE'
+execute_nested_base64(base64_string)
